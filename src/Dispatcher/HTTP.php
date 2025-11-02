e982ecbab836dcae261c25037f6ae3272edfdfe9
Evgeniy Kuvshinov <smpl@itmywork.com>
2025-11-03 02:30:27 +0500

    dispatcher http auto compile

--- src/Dispatcher/HTTP.php
+++ src/Dispatcher/HTTP.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Cekta\Framework\Dispatcher;
 
 use Cekta\Framework\Application;
+use Cekta\Framework\Configuration;
 use Cekta\Framework\ServiceProvider;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Container\ContainerInterface;
@@ -19,6 +20,7 @@ class HTTP
         $factory = new Psr17Factory();
         $worker = new PSR7Worker(Worker::create(), $factory, $factory, $factory);
         $configuration = Application::buildConfiguration($provider);
+        $this->compileContainer($configuration);
 
         while ($request = $worker->waitRequest()) {
             /** @var ContainerInterface $container */
@@ -31,5 +33,22 @@ class HTTP
                 $worker->getWorker()->error((string)$e);
             }
         }
+    }
+
+    private function compileContainer(Configuration $configuration)
+    {
+        file_put_contents(
+            $configuration->container_filename,
+            new \Cekta\DI\Compiler(
+                containers: $configuration->containers,
+                params: $configuration->params,
+                alias: $configuration->alias,
+                fqcn: $configuration->container_fqcn,
+            )->compile()
+        );
+
+        chmod($configuration->container_filename, 0777);
+
+        echo "$configuration->container_filename was compiled" . PHP_EOL;
     }
 }
