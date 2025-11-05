0ebe73ac29c7357278eac0b13af5bc728dda0812
Evgeniy Kuvshinov <smpl@itmywork.com>
2025-11-05 05:10:47 +0500

    fix filename for non exists

--- src/Application.php
+++ src/Application.php
@@ -27,7 +27,8 @@ class Application
         $app = new self();
         $app = $app_provider->register($app);
         return new Configuration(
-            container_filename: realpath($app->container_filename),
+            container_filename: file_exists($app->container_filename) ?
+                realpath($app->container_filename) : $app->container_filename,
             container_fqcn: $app->container_fqcn,
             containers: $app->containers,
             params: $app->params,
