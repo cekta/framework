4e4fa92ac6c20e7c8047777c561e136292cceee9
Evgeniy Kuvshinov <smpl@itmywork.com>
2025-11-16 00:10:42 +0500

    service provider chain

---
+++ src/ServiceProviderChain.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cekta\Framework;
+
+use Cekta\DI\Lazy;
+
+class ServiceProviderChain implements ServiceProvider
+{
+    /**
+     * @var ServiceProvider[]
+     */
+    private array $providers;
+    /**
+     * @var array<string, mixed|Lazy>
+     */
+    private array $params = [];
+
+    public function __construct(ServiceProvider ...$providers)
+    {
+        $this->providers = $providers;
+    }
+
+    public function params(): array
+    {
+        if (empty($this->params)) {
+            foreach ($this->providers as $provider) {
+                $this->params += $provider->params();
+            }
+        }
+        return $this->params;
+    }
+
+    public function register(): array
+    {
+        $result = [
+            'containers' => [],
+            'alias' => [],
+        ];
+        foreach ($this->providers as $provider) {
+            $r = $provider->register();
+            $result['containers'] = array_merge($result['containers'], $r['containers']);
+            $result['alias'] += $r['alias'];
+        }
+        return $result;
+    }
+}
