7a3c6fcd1758d1de7f2b78b1f340213085ba58d7
Evgeniy Kuvshinov <smpl@itmywork.com>
2026-01-27 03:02:32 +0500

    auto discovery cli command

--- src/CLI/Module.php
+++ src/CLI/Module.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Cekta\Framework\CLI;
 
 use ReflectionClass;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 
 class Module implements \Cekta\DI\Module
 {
@@ -20,8 +22,9 @@ class Module implements \Cekta\DI\Module
      */
     public function onCreate(string $encoded_module): array
     {
+        $state = json_decode($encoded_module, true);
         return [
-            ContainerCommandLoader::class . '$commandMap' => $this->command_map,
+            ContainerCommandLoader::class . '$commandMap' => [...($state['command_map'] ?? []), ...$this->command_map],
         ];
     }
 
@@ -30,10 +33,11 @@ class Module implements \Cekta\DI\Module
      */
     public function onBuild(string $encoded_module): array
     {
+        $state = json_decode($encoded_module, true);
         return [
             'entries' => [
                 Application::class,
-                ...(array_values($this->command_map)),
+                ...(array_values([...($state['command_map'] ?? []), ...$this->command_map])),
             ],
         ];
     }
@@ -43,6 +47,17 @@ class Module implements \Cekta\DI\Module
      */
     public function discover(ReflectionClass $class): void
     {
+        if (
+            $class->isSubclassOf(Command::class)
+            && $class->isInstantiable()
+            && !empty($attributes = $class->getAttributes(AsCommand::class))
+        ) {
+            foreach ($attributes as $attr) {
+                /** @var AsCommand $command */
+                $command = $attr->newInstance();
+                $this->state['command_map'][$command->name] = $class->name;
+            }
+        }
     }
 
     /**
