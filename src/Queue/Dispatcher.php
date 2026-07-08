a50e0199296939ddb3bd61a6b2597b9670918ec2
Evgeniy Kuvshinov <smpl@itmywork.com>
2026-07-09 00:10:32 +0500

    tmp task worker with fork

--- src/Queue/Dispatcher.php
+++ src/Queue/Dispatcher.php
@@ -6,6 +6,8 @@ namespace Cekta\Framework\Queue;
 
 use Cekta\Framework\Project;
 use Cekta\Queue\Postgres\Consumer;
+use Cekta\Queue\Postgres\TaskExecutor;
+use Cekta\Queue\Task;
 use Psr\Log\LoggerInterface;
 use Throwable;
 
@@ -14,7 +16,8 @@ class Dispatcher implements \Cekta\Framework\Dispatcher
     private bool $shouldStop = false;
 
     public function __construct(
-        private int $usleepAfterNotihing = 1000 * 300
+        private int $usleepAfterNotihing = 1000 * 300,
+        private int $taskMaxTimeSeconds = 60 * 60,
     ) {
     }
 
@@ -49,17 +52,23 @@ class Dispatcher implements \Cekta\Framework\Dispatcher
         pcntl_signal(SIGINT, $signalHandler);
         while (!$this->shouldStop) {
             try {
-                $task = $consumer->consume();
+                $task = $consumer->findNext();
                 if (null === $task) {
                     $logger->debug('nothing todo');
                     usleep($this->usleepAfterNotihing);
                     continue;
                 }
-                $logger->info("task {uuid} was handled by {handler} status is {status}", [
-                    'uuid' => $task->getUuid(),
-                    'handler' => $task->getHandler(),
-                    'status' => $task->getStatus(),
-                ]);
+                $pid = pcntl_fork();
+                if ($pid === -1) {
+                    $logger->emergency('cant create fork', [
+                        'task_uuid' => $task->getUuid()
+                    ]);
+                    exit(1);
+                }
+                if ($pid === 0) {
+                    $this->forkWork($project, $task);
+                }
+                $this->waitForChild($pid, $task, $logger);
             } catch (Throwable $e) {
                 $logger->emergency($e);
                 break;
@@ -67,5 +76,53 @@ class Dispatcher implements \Cekta\Framework\Dispatcher
         }
         $logger->info('worker stopped');
         return 0;
+    }
+
+    private function waitForChild(int $childPid, Task $task, LoggerInterface $logger): void
+    {
+        $startTime = time();
+        while (true) {
+            if (pcntl_waitpid($childPid, $status, WNOHANG) === $childPid) {
+                $logger->debug("child done", [
+                    'childPid' => $childPid,
+                    'hostname' => gethostname(),
+                    'task_uuid' => $task->getUuid(),
+                ]);
+                return;
+            }
+            if (time() - $startTime >= $this->taskMaxTimeSeconds) {
+                $logger->info('task timeout, send SIGKILL', [
+                    'task_uuid' => $task->getUuid(),
+                    'hostname' => gethostname(),
+                    'childPid' => $childPid,
+                    'handler' => $task->getHandler(),
+                ]);
+                posix_kill($childPid, SIGKILL);
+                pcntl_waitpid($childPid, $status);
+                $logger->debug('task waited');
+                return;
+            }
+            usleep(1000 * 100);
+        }
+    }
+
+    public function forkWork(Project $project, Task $task): void
+    {
+        try {
+            $container = $project->container();
+            /** @var LoggerInterface $logger */
+            $logger = $container->get(LoggerInterface::class);
+            /** @var TaskExecutor $taskExecutor */
+            $taskExecutor = $container->get(TaskExecutor::class);
+            $taskExecutor->execute($task);
+            $logger->info("task {uuid} was handled by {handler} status is {status}", [
+                'uuid' => $task->getUuid(),
+                'handler' => $task->getHandler(),
+                'status' => $task->getStatus(),
+            ]);
+        } catch (Throwable $e) {
+            exit(1);
+        }
+        exit(0);
     }
 }
